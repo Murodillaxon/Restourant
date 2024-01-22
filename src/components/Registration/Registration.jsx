e682950bf639613828fbe682b4ab143922925ce8
Murodillaxon <murka.surka.a4@gmail.com>
2024-01-22 16:01:52 +0500

    restarant

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,47 +1,46 @@
-import React, { useEffect, useRef, useState } from 'react'
-import './style.css'
+import React, { useEffect, useRef, useState } from 'react';
+import './style.css';
 
 const Registration = () => {
-  const name = useRef()
-  const mail = useRef()
-  const password = useRef()
+  const name = useRef();
+  const mail = useRef();
+  const password = useRef();
 
-
-  const [showHome, setShowHome] = useState(false)
-  const localSignUp = localStorage.getItem('signUp')
+  const [showHome, setShowHome] = useState(false);
+  const localSignUp = localStorage.getItem('signUp');
   useEffect(() => {
     if (localSignUp) {
-      setShowHome(true)
+      setShowHome(true);
     }
-  })
+  }, [localSignUp]);
 
   const handleClick = () => {
-    console.log(name.current.value, mail.current.value, password.current.value);
     if (name.current.value && mail.current.value && password.current.value) {
-      localStorage.setItem('name', name.current.value)
-      localStorage.setItem('mail', mail.current.value)
-      localStorage.setItem('password', password.current.value)
-      localStorage.setItem('signUp', mail.current.value)
-      alert("Успешное регистрация")
+      localStorage.setItem('name', name.current.value);
+      localStorage.setItem('mail', mail.current.value);
+      localStorage.setItem('password', password.current.value);
+      localStorage.setItem('signUp', mail.current.value);
+      alert('Успешная регистрация');
     }
-  }
+  };
+
   return (
-    <div className='Menu'>
-      {showHome? window.location = '/' :
-      <div className='container'>
-        <div className='input_space'>
-          <input type="text" placeholder='Name' ref={name} />
-        </div >
-        <div className='input_space'>
+    <div className={showHome ? 'hidden' : 'registration-container'}>
+      <div className='registration-form'>
+        <h2>Регистрация</h2>
+        <div className='input-space'>
+          <input type="text" placeholder='Имя' ref={name} />
+        </div>
+        <div className='input-space'>
           <input type="text" placeholder='Email' ref={mail} />
         </div>
-        <div className='input_space'>
-          <input type="password" placeholder='Name' ref={password} />
+        <div className='input-space'>
+          <input type="password" placeholder='Пароль' ref={password} />
         </div>
-        <button onClick={handleClick}>Sign Up</button>
-      </div>}
+        <button onClick={handleClick}>Зарегистрироваться</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Registration
+export default Registration;
